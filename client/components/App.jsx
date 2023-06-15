06f248cda0fa8cdcf32ff68736ffcade4dd6c62e
timgall <119544130+timgall@users.noreply.github.com>
2023-06-15 08:53:14 -0500

    Tim's Updates

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,25 +1,36 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const App = () => {
-  const [tasks, setTasks] = useState([]);
+const YouTubeWebsite = () => {
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetch("/api/tasks")
-      .then((res) => res.json())
-      .then((tasks) => {
-        setTasks(tasks);
-      });
+    // Insert our Youtube API here:
+    fetch("")
+      .then((response) => response.json())
+      .then((data) => setVideos(data))
+      .catch((error) => console.log(error));
   }, []);
 
   return (
-    <main>
-      {tasks.map((task) => (
-        <span className="task" key={task.id}>
-          {task.description}
-        </span>
-      ))}
-    </main>
+    <div>
+      <header>
+        <h1>Welcome to Office Avengers' YouTube Website</h1>
+      </header>
+
+      <div id="videos">
+        {videos.map((video) => (
+          <div className="video" key={video.id}>
+            <img
+              className="thumbnail"
+              src={video.thumbnail}
+              alt={video.title}
+            />
+            <div className="title">{video.title}</div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
-export default App;
+export default YouTubeWebsite;
--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App.jsx";
+// import { createRoot } from "react-dom/client";
+// import App from "./components/App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
-// TEST FOR COMMIT
+// createRoot(document.getElementById("root")).render(<App />);
+// // TEST FOR COMMIT
+import ReactDOM from "react-dom";
+import YouTubeWebsite from "./YouTubeWebsite";
+import "./styles.css";
+
+ReactDOM.render(<YouTubeWebsite />, document.getElementById("root"));
