9ca07ae7bb74c88a102052256ff647c75001bf74
s p <sp@ss-macbook-air.lan>
2023-06-16 15:40:32 -0400

    header searchbar returns array

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,20 +4,20 @@ import Header from './Header.jsx'
 import { Routes, useNavigate, Route } from 'react-router-dom'
 
 const App = () => {
-    //   useEffect(() => {
-    //     getVideos();
-    //   }, []);
+    // useEffect(() => {
+    //     getVideos()
+    // }, [])
 
     // const getVideos = () => {
-    //   axios
-    //     .get("/api/videos")
-    //     .then((response) => {
-    //       console.log(response.data);
-    //     })
-    //     .catch((error) => {
-    //       console.error("Error fetching videos", error);
-    //     });
-    // };
+    //     axios
+    //         .get('/api/videos')
+    //         .then((response) => {
+    //             console.log(response.data)
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error fetching videos', error)
+    //         })
+    // }
 
     return (
         <>
